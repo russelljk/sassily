9923b31e99e39c8fe024d7b993f7155d8ee4b10c
Russell Kyle <russell.j.kyle@gmail.com>
2013-09-11 20:36:52 -0500

    Update setup script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sassily',
+      version='0.1',
+      description='Management commands for using sass and Django together.',
+      author='Russell Kyle',
+      author_email='russell.j.kyle@gmail.com',
+      url='http://russellkyle.com/django-sassily/',
+      download_url='https://github.com/russelljk/django-sassily/archive/master.zip',
+      packages=['sassily'],
+      keywords=['Django', 'SASS', 'CSS']
+)
